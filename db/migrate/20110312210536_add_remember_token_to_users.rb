bc950d5f015fc3b02ca33ecd0d5b0cf3f567be62
Yuri Omelchuk <jurgen@upscript.com>
2011-03-13 01:57:27 +0200

    turn remember_me on

--- db/migrate/20110312210536_add_remember_token_to_users.rb
+++ db/migrate/20110312210536_add_remember_token_to_users.rb
@@ -1,6 +1,6 @@
 class AddRememberTokenToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :remember_token,:string
+    add_column :users, :remember_token, :string
   end
 
   def self.down
